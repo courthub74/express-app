90c733ba821558376efb8ae5645f8d64be56945f
Courdevelops <courdevelops@gmail.com>
2022-01-12 17:28:45 -0500

    IndexPage

---
+++ server.js
@@ -0,0 +1,39 @@
+//Basic Output
+// console.log("Hello World");
+
+//Require express library that was downloaded
+const express = require("express")
+
+//set up the server with app variable calling express as a function
+const app = express()
+
+//Tell app to use view engine
+app.set('view engine', 'ejs')
+
+//set up routes ("/" - root path)
+app.get("/", (req, res) => {
+
+    //sends a status
+    //res.sendStatus(200).send("OK")
+
+    //downloads the server.js file
+    //res.download("server.js")
+
+    //to pass a string to console
+    console.log("Hello World")
+
+    //just pass a string
+    //res.send("Hello World")
+
+    //pass a json
+    //res.json({ message: "Hello World"})
+
+    //renders the index.html file
+    res.render('index', {text: "...And this is more Text"})
+    //{text:} adds to the page
+    
+
+})
+
+//make servern run pass through port #
+app.listen(3000)
